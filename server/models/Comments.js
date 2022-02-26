152d89cf84ed9ef7a53fc2d3e1c2df820f904df0
DavisRansom <davisransom.vb@gmail.com>
2022-02-26 10:35:18 -0800

    Config file

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -2,22 +2,19 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
-const postSchema = new Schema(
+const commentSchema = new Schema(
   {
       id: {
         type: Number
       },
-    title: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 80
-    },
     text: {
       type: String,
       required: true,
       minlength: 1,
-      maxlength: 400
+      maxlength: 200
+    },
+    contentType: {
+        type: Dropdown
     },
     createdAt: {
       type: Date,
@@ -56,4 +53,4 @@ postSchema.virtual('reactionCount').get(function() {
 
 const Post = model('Post', postSchema);
 
-module.exports = Post;
+module.exports = Comments;
