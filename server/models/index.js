135bce5e82095ccfb9d1ccf6caefac831c5330e9
DavisRansom <davisransom.vb@gmail.com>
2022-02-26 13:09:57 -0800

    Added schema info for resolvers and typedefs

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,5 @@ const User = require('./User');
 const Post = require('./Post');
 const Community = require('./Community')
 const Comments = require('./Comments')
-const Reaction = require('./Reaction')
 
-module.exports = { User, Post, Community, Comments, Reaction };
+module.exports = { User, Post, Community, Comments };
