5ccbe761c49073ea3d80fb6ff00d765fab87d558
DavisRansom <davisransom.vb@gmail.com>
2022-02-25 19:24:34 -0800

    Initial commit of post Schema

---
+++ server/models/Post.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema(
+  {
+    postText: {
+      type: String,
+      required: 'Leave your post here!',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
