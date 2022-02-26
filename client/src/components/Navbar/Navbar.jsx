9693b269512b7879abf3ae91fbc4fcd177a76480
steph <sjapodaca1103@gmail.com>
2022-02-25 23:46:51 -0800

    created and added to navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css"
+export default function Navbar() {
+    return (
+        <div className="navbarContainer">
+            <div className="navbarLeft">
+                <span className="logo">Agora</span>
+            </div>
+            <div className="navbarCenter">
+                <div className="searchBar">
+                    <Search />
+                    <input placeholder="Search Agora" className="searchInput" />
+
+
+                </div>
+            </div>
+            <div className="navbarRight">
+                <div className="navbarLinks">
+                    <span className="navbarLink">Create Post</span>
+                    <span className="navbarlink">Profile/Signup</span>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
