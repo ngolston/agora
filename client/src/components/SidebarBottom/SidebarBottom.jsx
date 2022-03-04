e3afddb872818856333cd6085c6556a2ec5b18d6
steph <sjapodaca1103@gmail.com>
2022-03-04 08:50:42 -0800

    redo react

---
+++ client/src/components/SidebarBottom/SidebarBottom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./SidebarBottom.css"
+export default function SidebarBottom() {
+    return (
+        <div>
+            side bar SidebarBottom
+        </div>
+    )
+}
