04c6cab6df6c1c8261d4755b9b32643e9f941e07
Nicholas G <nick2123@gmail.com>
2022-03-04 07:25:19 -0800

    Some of the Auth done

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,19 @@
 import React from "react";
-import Home from "./pages/home/home";
-import { useState, useEffect } from "react";
-import axios from 'axios';
-import PostFormModalCon from "../src/components/Post/PostFormModalCon"
-// import Modal from "../src/components/Modal/Modal";
-// import { AccountCircleIcon } from '@mui/icons-material/AccountCircle';
+import { BrowserRouter, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
-  const [showAuthModal, setShowAuthModal] = useState(false);
-  const [showPostFormModal, setShowPostFormModal] = useState(false);
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-
-    axios.get('http://localhost:4000/user', { withCredentials: true })
-      .then(response => setUser(response.data));
-
-  }, []);
-  function logout() {
-    axios.post('http://localhost:4000/logout', {}, { withCredentials: true })
-      .then(() => setUser({}));
-  }
-
+const App = () => {
   return (
-    <AuthModalCon.Provider value={{ show: showAuthModal, setShow: setShowAuthModal }}>
-      <PostFormModalCon.Provider value={{ show: showPostFormModal, setShow: setShowPostFormModal }}>
-        <UserContext.Provider value={{ ...user, logout, setUser }}>
-          <Home />
-        </UserContext.Provider>
-      </PostFormModalCon.Provider>
-    </AuthModalCon.Provider>
+    <div>
+      <BrowserRouter>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/dashboard" exact component={Dashboard} />
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
-
-
-
-
-
