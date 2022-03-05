85dd7e135939f6f969fdf6d90aa943b14af16151
Nicholas G <nick2123@gmail.com>
2022-03-05 10:45:43 -0800

    Done with auth2

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,4 @@
 const { Schema, model } = require("mongoose");
-const crypto = require("crypto");
-const { v4: uuidv4 } = require("uuid");
 
 const userSchema = new Schema(
   {
@@ -39,36 +37,6 @@ const userSchema = new Schema(
   },
   { collection: "user-data" }
 );
-
-// // virtual field
-// userSchema
-//   .virtual("password")
-//   .set(function (password) {
-//     // create temporary variable called _password
-//     this._password = password;
-//     // generate a timestamp
-//     this.salt = uuidv4();
-//     // encryptPassword()
-//     this.hashed_password = this.encryptPassword(password);
-//   })
-//   .get(function () {
-//     return this._password;
-//   });
-
-// // methods
-// userSchema.methods = {
-//   encryptPassword: function (password) {
-//     if (!password) return "";
-//     try {
-//       return crypto
-//         .createHmac("sha1", this.salt)
-//         .update(password)
-//         .digest("hex");
-//     } catch (err) {
-//       return "";
-//     }
-//   },
-// };
 
 userSchema.virtual("reactionCount").get(function () {
   return this.posts.reduce((a, b) => a + b, 0);
