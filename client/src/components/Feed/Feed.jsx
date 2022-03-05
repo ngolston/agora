98b24f0cbccd8a554aa6aec369578818268445e2
Nicholas G <62343903+ngolston@users.noreply.github.com>
2022-03-05 13:37:18 -0800

    not using feed right now

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import "./Feed.css"
+// import React from 'react'
+// import "./Feed.css"
 
-import Post from '../Post/Post'
-export default function Feed() {
-    return (
-        <div className="feed">
-            <div className="feedBoardWrap">
+// import Post from '../Post/Post'
+// export default function Feed() {
+//     return (
+//         <div className="feed">
+//             <div className="feedBoardWrap">
 
-                <Post />
+//                 <Post />
 
 
-            </div>
+//             </div>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
