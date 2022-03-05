abc3030d2978ccce5fcbdd69b7df3b66da55ac13
steph <sjapodaca1103@gmail.com>
2022-03-05 13:03:04 -0800

    saved changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,10 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
->>>>>>> 048bb9e1387fa39d3fdc8a65e7f8227b4bbbbb77
 
 ReactDOM.render(<App />, document.getElementById("root"));
