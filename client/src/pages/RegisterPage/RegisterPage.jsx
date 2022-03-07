9fa73208ddf0a1f02dd12214dc9faebc78dedc34
Nicholas G <62343903+ngolston@users.noreply.github.com>
2022-03-06 16:19:15 -0800

    Update RegisterPage.jsx
    temp fix

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -273,108 +273,109 @@ export default function Register({ setOpenRegister }) {
 
 // }
 
+/////////////////////////////////////////// Nick Keep /////////////////////////////////////////////////////////////////////////////////////
+// import { useState } from "react";
+// import Navbar from "../../components/Navbar/Navbar";
+// import { useHistory } from "react-router-dom";
+// import Form from "react-bootstrap/Form"
+// import Button from "react-bootstrap/Button"
 
-import { useState } from "react";
-import Navbar from "../../components/Navbar/Navbar";
-import { useHistory } from "react-router-dom";
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-
-function App() {
-    const history = useHistory();
+// function App() {
+//     const history = useHistory();
   
-    const [userName, setUserName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+//     const [userName, setUserName] = useState("");
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
   
-    async function registerUser(event) {
-      event.preventDefault();
+//     async function registerUser(event) {
+//       event.preventDefault();
   
-      const response = await fetch("http://localhost:3001/api/Register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userName,
-          email,
-          password,
-        }),
-      });
+//       const response = await fetch("http://localhost:3001/api/Register", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           userName,
+//           email,
+//           password,
+//         }),
+//       });
   
-      const data = await response.json();
+//       const data = await response.json();
   
-      if (data.status === "ok") {
-        history.push("/login");
-      }
-    }
+//       if (data.status === "ok") {
+//         history.push("/login");
+//       }
+//     }
 
-    return (
-        <div>
-            <>
-                <Navbar />
-            </>
-            <div className="modalBG">
-                <div className="modalContainer">
-                <Form onSubmit={registerUser}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>UserName</Form.Label>
-                        <Form.Control value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder="Enter UserName" />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
+//     return (
+//         <div>
+//             <>
+//                 <Navbar />
+//             </>
+//             <div className="modalBG">
+//                 <div className="modalContainer">
+//                 <Form onSubmit={registerUser}>
+//                     <Form.Group className="mb-3" controlId="formBasicEmail">
+//                         <Form.Label>UserName</Form.Label>
+//                         <Form.Control value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder="Enter UserName" />
+//                         <Form.Text className="text-muted">
+//                         </Form.Text>
+//                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+//                     <Form.Group className="mb-3" controlId="formBasicEmail">
+//                         <Form.Label>Email address</Form.Label>
+//                         <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+//                         <Form.Text className="text-muted">
+//                             We'll never share your email with anyone else.
+//                         </Form.Text>
+//                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    </Form.Group>
-                    <Button variant="primary" type="submit" value="Register">
-                        Submit
-                    </Button>
-                    </Form>
-
-
+//                     <Form.Group className="mb-3" controlId="formBasicPassword">
+//                         <Form.Label>Password</Form.Label>
+//                         <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+//                     </Form.Group>
+//                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+//                     </Form.Group>
+//                     <Button variant="primary" type="submit" value="Register">
+//                         Submit
+//                     </Button>
+//                     </Form>
 
 
 
 
 
 
-                    {/* <Form onSubmit={registerUser}>
-                            <h1>Create User Profile</h1>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-                        </div>
-                        <div className="body">
-                            <p>choose a unique username and secure password in order to continue</p>
-                        </div>
-                        <div className="footer">
-                            <button >Cancel</button>
-                            <button>Submit</button>
-
-                        </div>
-                    </form> */}
-                </div>
-            </div>
-        </div>
 
 
-    );
-}
+//                     {/* <Form onSubmit={registerUser}>
+//                             <h1>Create User Profile</h1>
+//                             <Form.Group className="mb-3" controlId="formBasicEmail">
+//                         <Form.Label>Email address</Form.Label>
+//                         <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+//                         <Form.Text className="text-muted">
+//                             We'll never share your email with anyone else.
+//                         </Form.Text>
+//                     </Form.Group>
+//                         </div>
+//                         <div className="body">
+//                             <p>choose a unique username and secure password in order to continue</p>
+//                         </div>
+//                         <div className="footer">
+//                             <button >Cancel</button>
+//                             <button>Submit</button>
 
-export default App;
+//                         </div>
+//                     </form> */}
+//                 </div>
+//             </div>
+//         </div>
+
+
+//     );
+// }
+
+// export default App;
+/////////////////////////////////////////// Nick Keep /////////////////////////////////////////////////////////////////////////////////////
